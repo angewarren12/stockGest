fd79b13f49dcdd299c6632f5527ec0883e85f964
ange warren <angedesirecamara@gmail.com>
2022-08-30 13:26:39 +0000

    my first commit

---
+++ src/main/java/ci/smile/stockGest/StockGestApplication.java
@@ -0,0 +1,13 @@
+package ci.smile.stockGest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockGestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockGestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ci/smile/stockGest/StockGestApplicationTests.java
@@ -0,0 +1,13 @@
+package ci.smile.stockGest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockGestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
